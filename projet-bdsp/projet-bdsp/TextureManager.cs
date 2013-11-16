4471cde6d7a91dc79faf07dc756ec735c2f6f6c4
benjamin310395 <benjamin310395@free.fr>
2013-11-16 01:24:15 +0100

    gdgdftetgyrth
    gfdgd

--- projet-bdsp/projet-bdsp/TextureManager.cs
+++ projet-bdsp/projet-bdsp/TextureManager.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace projet_bdsp
 {
-    class TextureManager
-    {
-    }
+    // ATTENTION au mot clef 'static'
+ public static class TexturesManager
+ {
+ public static Texture2D GameBackground,
+ GameOver,
+ Piece,
+ PieceBlocked ;
+
+ public static void LoadContent(ContentManager content)
+ {
+ // TODO: Charger les 4 textures
+ }
+}
 }
